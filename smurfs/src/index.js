2ee91e5e2f659b4bf4908dc76ca7c1a43ad599ab
ampersand <ampersand@ampersands-MacBook-Pro.local>
2019-09-13 09:48:41 -0700

    set up 2 take 2

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -6,7 +6,7 @@ import App from "./components/App";
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-import {reducer} from './components/reducers/reducer';
+import {reducer} from './reducers/reducer';
 
 const store = createStore(reducer);
 
